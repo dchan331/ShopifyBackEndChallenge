073a00835cf6eb85444d880f90993497b88686a7
dchan331 <dchan331@gmail.com>
2017-08-13 20:56:33 -0700

    loops through all pages and grabs all invalid entries

--- routes/routes.js
+++ routes/routes.js
@@ -1,108 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../model/user');
-const func = require('./app.js');
-const dataSet = func.fileReader('./listing-details.csv');
 const _ = require('underscore');
 const bodyParser = require('body-parser');
-var fs = require('fs');
 router.use(bodyParser.urlencoded({ extended: true }))
 
-var cookieParser = require('cookie-parser')
-router.use(cookieParser())
-
 // mongoose configuration
-const mongoose = require('mongoose');
-
-if (! fs.existsSync('./env.sh')) {
-  throw new Error('env.sh file is missing');
-}
-if (! process.env.MONGODB_URI) {
-  throw new Error("MONGODB_URI is not in the environmental variables. Try running 'source env.sh'");
-}
-mongoose.connection.on('connected', function() {
-  console.log('Success: connected to MongoDb!');
-});
-mongoose.connection.on('error', function() {
-  console.log('Error connecting to MongoDb. Check MONGODB_URI in env.sh');
-  process.exit(1);
-});
-mongoose.connect(process.env.MONGODB_URI);
+// const mongoose = require('mongoose');
+//
+// if (! process.env.MONGODB_URI) {
+//   throw new Error("MONGODB_URI is not in the environmental variables. Try running 'source env.sh'");
+// }
+// mongoose.connection.on('connected', function() {
+//   console.log('Success: connected to MongoDb!');
+// });
+// mongoose.connection.on('error', function() {
+//   console.log('Error connecting to MongoDb. Check MONGODB_URI in env.sh');
+//   process.exit(1);
+// });
+// mongoose.connect(process.env.MONGODB_URI);
 
 // YOUR API ROUTES HERE
-
-router.get('/listings', function(req, res){
-  if(req.cookies.api_key){
-    req.query.min_price = req.query.min_price || 0;
-    req.query.max_price = req.query.max_price || Infinity;
-    req.query.min_bed = req.query.min_bed || 0;
-    req.query.max_bed = req.query.max_bed || Infinity;
-    req.query.min_bath = req.query.min_bath || 0;
-    req.query.max_bath = req.query.max_bath || Infinity;
-    const filteredData = dataSet.filter((home) => {
-      return Number(home.price) >= Number(req.query.min_price) &&
-      Number(home.price) <= Number(req.query.max_price) &&
-      Number(home.bedrooms) >= Number(req.query.min_bed) &&
-      Number(home.bedrooms) <= Number(req.query.max_bed) &&
-      Number(home.bathrooms) >= Number(req.query.min_bath) &&
-      Number(home.bathrooms) <= Number(req.query.max_bath);
-    })
-    res.send(func.GeoJSON(filteredData));
-  }else{
-    res.send('Cookies dont exist')
-  }
-});
-
-router.post('/register', function(req, res){
-  const username = req.body.username;
-  const password = req.body.password;
-  const apikey = func.randomString(10)
-  User.findOne({username: username})
-    .then((user) => {
-      if(_.isEmpty(user)){
-        const newUser = new User({
-          api_key: apikey,
-          username: username,
-          password: password
-        });
-        res.cookie('api_key', apikey, {expire: 360000 + Date.now()});
-        newUser.save((err) => {
-          if(err){
-            res.send('Failed to save User')
-          }else{
-            res.send('Success')
-          }
-        })
-      }else{
-        res.status('403').send('User already exists')
-      }
-    })
-})
-
-
-router.post('/retrieve_key', function(req, res){
-  User.findOne({username: req.body.username, password: req.body.password})
-    .then((resp) => res.send(resp.api_key))
-});
-
-router.post('/refresh_key', function(req, res){
-  const newKey = func.randomString(10);
-  User.findOneAndUpdate({api_key: req.body.api_key}, {api_key: newKey})
-    .then((resp) => {
-      User.findOne({api_key: newKey})
-      .then((resp2) => (
-        res.send(resp2.api_key)
-      ))
-    })
-
-});
-
-// SAMPLE ROUTE
-router.use('/users', (req, res) => {
-    res.json({ success: true });
-});
-
-
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,13 +3,37 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const api = require('./routes/routes');
-const axios = require('axios')
+const axios = require('axios');
+const _ = require('underscore');
 
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (request, response) => {
-  response.sendFile(__dirname + '/public/index.html'); // For React/Redux
+app.get('/', (req, res) => {
+  axios.get('https://backend-challenge-winter-2017.herokuapp.com/customers.json')
+  .then(response => {
+    return response.data.pagination
+  })
+  .then(resp => {
+    var length = Math.ceil(resp.total / resp.per_page);
+    let array = [];
+    for(var i = 0 ; i < length ; i ++){
+      array.push(axiosLoop(i))
+    }
+    return Promise.all(array)
+  })
+  .then(resp =>{
+    let finalArray = []
+    resp.forEach((index) => {
+      index.forEach((invalid) => {
+        finalArray.push(invalid)
+      })
+    })
+    res.send({"invalid_customers":finalArray})
+  })
+  .catch(err => {
+    res.send('error');
+  })
 });
+
 
 app.use('/api', api);
 
@@ -18,5 +42,62 @@ app.listen(PORT, error => {
   ? console.error(error)
   : console.info(`==> 🌎 Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`);
 });
+
+function axiosLoop(index){
+  let responseArray = [];
+  return axios.get('https://backend-challenge-winter-2017.herokuapp.com/customers.json',{
+    params: {
+      page: index + 1
+    }
+  })
+  .then(response => {
+    return filterData(response)
+  })
+  .then(resp => {
+    if(resp.length > 0){
+      for(var j = 0 ; j < resp.length ; j ++){
+        responseArray.push(resp[j])
+      }
+    }
+    return responseArray;
+  })
+}
+
+function Validation(attribute, method){
+  let bool = false
+  if(!method.required){
+    return false
+  }else if(attribute === null){
+    bool = true
+  }else if(method.hasOwnProperty('length') && attribute.length < method.length.min){
+    bool = true
+  }else if(method.hasOwnProperty('length') && attribute.length > method.length.max){
+    bool = true
+  }else if((method.hasOwnProperty('type') && typeof attribute !== method["type"])){
+    bool = true
+  }
+  return bool
+}
+
+function filterData(response){
+  const data = response.data;
+  const methods = data.validations;
+  let invalidArray = [];
+  data.customers.forEach(customer => {
+    let customerObj = {"id": customer.id, "invalid_fields":[]}
+    methods.forEach( method => {
+      const key = Object.keys(method)[0];
+      if(Validation(customer[key], method[key])){
+        var IFArray = customerObj["invalid_fields"];
+        customerObj["invalid_fields"] = IFArray.concat([key])
+      }
+    })
+
+    if(customerObj["invalid_fields"].length !== 0){
+      invalidArray.push(customerObj)
+    }
+  })
+  return invalidArray;
+}
 
 module.exports = app;
